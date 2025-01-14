93aa131afd7188455cee909228ebf491f45090b4
asqaqtemir <78165333+asqaqtemir@users.noreply.github.com>
2025-01-14 14:55:25 +0500

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,10 @@
+// Түрлендіру ережелері
+const rules = [
+    { pattern: /^само/, replacement: "өз" },
+    { pattern: /ость$/, replacement: "лық" },
+    { pattern: /ение$/, replacement: "у" },
+    { pattern: /ство$/, replacement: "шылық" },
+    { pattern: /^пере/, replacement: "қайта" },
+    { pattern: /^под/, replacement: "асты" },
+    { pattern: /^над/, replacement: "үсті" }
+];
