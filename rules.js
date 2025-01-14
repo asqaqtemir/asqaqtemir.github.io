782c86c686f35012def9f3fe30296039ea9635cc
asqaqtemir <78165333+asqaqtemir@users.noreply.github.com>
2025-01-14 15:20:02 +0500

    Update rules.js

--- rules.js
+++ rules.js
@@ -10,11 +10,38 @@ const rules = [
 
      { pattern: /логия$/, replacement: "тану" },
     { pattern: /лог$/, replacement: "танушы" },
-    { pattern: /^био/, replacement: "тіршілік" },
+    { pattern: /^био/, replacement: "өмір" },
     { pattern: /^гео/, replacement: "жер" },
       { pattern: /графия$/, replacement: "жазба" },
     { pattern: /лог$/, replacement: "танушы" },
     { pattern: /^гидро/, replacement: "су" },
     { pattern: /^микро/, replacement: "кіші" },
+{ pattern: /^зоо/, replacement: "аң" },
+    { pattern: /^изо/, replacement: "тең" },
+     { pattern: /^авто/, replacement: "өз" },
+    { pattern: /^архео/, replacement: "көне" },
+{ pattern: /^аэро/, replacement: "ауа" },
+    { pattern: /^моно/, replacement: "бір" },
+{ pattern: /^орфо/, replacement: "дұрыс" },
+    { pattern: /^поли/, replacement: "көп" },
+{ pattern: /теле/, replacement: "алыс" },
+    { pattern: /^типо/, replacement: "мөр" },
+     { pattern: /^топо/, replacement: "орын" },
+    { pattern: /^томо/, replacement: "кесік" },
+{ pattern: /^фоно/, replacement: "үн" },
+    { pattern: /^фото/, replacement: "жарық" },
+
+    { pattern: /^этно/, replacement: "ел" },
+    { pattern: /^ихтио/, replacement: "балық" },
+     { pattern: /^калли/, replacement: "сұлу" },
+    { pattern: /^ксило/, replacement: "ағаш" },
+{ pattern: /^порно/, replacement: "бүлік" },
+    { pattern: /^хорео/, replacement: "би" },
+{ pattern: /^термо/, replacement: "жылы" },
+    { pattern: /^макро/, replacement: "ұлы" },
+
+
+
+
     
 ];
